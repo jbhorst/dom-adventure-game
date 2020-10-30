373168713c6a33caf5ce0714af01e40e7a281b29
Jonah Horst <jbhorst@yahoo.com>
2020-10-30 19:43:48 -0400

    Nearly done added comments on js, html, and css. still need to fix text input box problem and possibly edit some styles.

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,9 @@
  */
 
 // document.querySelector('#game').textContent = 'DOM Adventure Game';
+
+
+// Assign variables //
 let game = document.getElementById("game");
 let textEl = document.getElementById("text");
 let btnContainer = document.getElementById("choice-buttons");
@@ -19,6 +22,8 @@ let answer = document.getElementById("answer");
 let answer2 = document.getElementById("answer2");
 let answer3 = document.getElementById("answer3");
 
+// Create function called start that includes click event listeners //
+// When you click the html button it either goes to the math functions or riddle function //
 function start() {
   btnEl[0].addEventListener("click", () => {
     return math1();
@@ -28,6 +33,10 @@ function start() {
   });
 };
 
+// Create a function called math1 to ask the first math question //
+// Used a keydown event listener so the user can press a key for the answer //
+// Create branch that returns the math1BridgeChoice function if the answer is correct and the death function if wrong //
+// Used the textContent to change the text //
 const math1 = function() {
   textEl.textContent = "You walk over the bridge to another rock. The bridge behind you disappears. Again, you see the troll in between two bridges. You may only choose which to cross once you have answered the troll's question. The troll looks at you and asks, what is 2 times 2? (Press key a, b, c, or d to answer)";
   btnContainer.style.display = "none";
@@ -41,6 +50,10 @@ const math1 = function() {
   });
 };
 
+// Create a function called math1BridgeChoice to ask with bridge the user wants to cross //
+// Used a click event listener so the user can the button for the direction they wish to go //
+// Returns the math2 function if the user chooses the right direction and the death2 function if wrong //
+// Used the textContent to change the text //
 const math1BridgeChoice = function() {
   textEl.textContent = "Well, aren't you smart. Now, which path will you take?";
   btnContainer.style.display = "block";
@@ -53,6 +66,10 @@ const math1BridgeChoice = function() {
   });
 };
 
+// Create a function called math2 to ask the second math question //
+// Used a keydown event listener so the user can press a key for the answer //
+// Create branch that returns the math2BridgeChoice function if the answer is correct and the death function if wrong //
+// Used the textContent to change the text //
 const math2 = function() {
   textEl.textContent = "You walk over the bridge to another rock. The bridge behind you disappears. Again, you see the troll in between two bridges. You may only choose which to cross once you have answered the troll's question. The troll looks at you and asks, what is 6/2(1+2)? (Press key a, b, c, or d to answer)";
   btnContainer.style.display = "none";
@@ -71,6 +88,10 @@ const math2 = function() {
   });
 };
 
+// Create a function called math2BridgeChoice to ask with bridge the user wants to cross //
+// Used a click event listener so the user can the button for the direction they wish to go //
+// Returns the math3 function if the user chooses the right direction and the death2 function if wrong //
+// Used the textContent to change the text //
 const math2BridgeChoice = function() {
   textEl.textContent = "I thought for sure you would answer that one wrong! No matter, you won't get past the final question. Now which path will you take?";
   btnContainer.style.display = "block";
@@ -83,6 +104,10 @@ const math2BridgeChoice = function() {
   });
 };
 
+// Create a function called math3 to ask the third math question //
+// Used a keydown event listener so the user can press a key for the answer //
+// Create branch that returns the math3BridgeChoice function if the answer is correct and the death function if wrong //
+// Used the textContent to change the text //
 const math3 = function() {
   textEl.textContent = "You walk over the bridge to another rock. The bridge behind you disappears. Again, you see the troll in between two bridges. You may only choose which to cross once you have answered the troll's question. The troll looks at you and asks, If 1 = 4, 2 = 16, and 3 = 64, what does 4 equal? (Press key a, b, c, or d to answer)";
   btnContainer.style.display = "none";
@@ -100,6 +125,10 @@ const math3 = function() {
   });
 };
 
+// Create a function called math3BridgeChoice to ask with bridge the user wants to cross //
+// Used a click event listener so the user can the button for the direction they wish to go //
+// Returns the winner function if the user chooses the right direction and the death2 function if wrong //
+// Used the textContent to change the text //
 const math3BridgeChoice = function() {
   textEl.textContent = "Well done. You're smarter than you look. Now which path will you take? Choose the correct path and you may leave my realm.";
   btnContainer.style.display = "block";
@@ -112,6 +141,10 @@ const math3BridgeChoice = function() {
   });
 };
 
+// Create a function called riddle1 to ask the first riddle //
+// Used a change event listener //
+// Create branch that returns the riddle1BridgeChoice function if the text input answer is correct and the death function if wrong //
+// Used the textContent to change the text //
 const riddle1 = function() {
   textEl.textContent = "You walk over the bridge to another rock. The bridge behind you disappears. Again, you see the troll in between two bridges. You may only choose which to cross once you have answered the troll's question. The troll looks at you and asks, what is full of holes yet still holds water? (Use one word answers and lowercase letters only.)";
   btnContainer.style.display = "none";
@@ -125,6 +158,10 @@ const riddle1 = function() {
   });
 };
 
+// Create a function called riddle1BridgeChoice to ask with bridge the user wants to cross //
+// Used a click event listener so the user can the button for the direction they wish to go //
+// Returns the riddle2 function if the user chooses the right direction and the death2 function if wrong //
+// Used the textContent to change the text //
 const riddle1BridgeChoice = function() {
   textEl.textContent = "Don't give yourself too much credit. That was an easy one. Now which path will you take?";
   btnContainer.style.display = "block";
@@ -138,6 +175,10 @@ const riddle1BridgeChoice = function() {
   });
 };
 
+// Create a function called riddle2 to ask the second riddle //
+// Used a change event listener //
+// Create branch that returns the riddle2BridgeChoice function if the text input answer is correct and the death function if wrong //
+// Used the textContent to change the text //
 const riddle2 = function() {
   textEl.textContent = "You walk over the bridge to another rock. The bridge behind you disappears. Again, you see the troll in between two bridges. You may only choose which to cross once you have answered the troll's question. The troll looks at you and asks, I am tall when I am young, and I am short when I am old. What am I? (Use one word answers and lowercase letters only.)";
   btnContainer.style.display = "none";
@@ -153,6 +194,10 @@ const riddle2 = function() {
   });
 };
 
+// Create a function called riddle2BridgeChoice to ask with bridge the user wants to cross //
+// Used a click event listener so the user can the button for the direction they wish to go //
+// Returns the riddle3 function if the user chooses the right direction and the death2 function if wrong //
+// Used the textContent to change the text //
 const riddle2BridgeChoice = function() {
   textEl.textContent = "You will not get past my final riddle! Now which path will you take?";
   btnContainer.style.display = "block";
@@ -166,6 +211,10 @@ const riddle2BridgeChoice = function() {
   });
 };
 
+// Create a function called riddle3 to ask the third riddle //
+// Used a change event listener //
+// Create branch that returns the riddle3BridgeChoice function if the text input answer is correct and the death function if wrong //
+// Used the textContent to change the text //
 const riddle3 = function() {
   textEl.textContent = "You walk over the bridge to another rock. The bridge behind you disappears. Again, you see the troll in between two bridges. You may only choose which to cross once you have answered the troll's question. The troll looks at you and asks, I float in the sky, I cover the land; I can carry your weight or slip through your hand. What am I? (Use one word answers and lowercase letters only.)";
   btnContainer.style.display = "none";
@@ -180,6 +229,10 @@ const riddle3 = function() {
   });
 };
 
+// Create a function called riddle3BridgeChoice to ask with bridge the user wants to cross //
+// Used a click event listener so the user can the button for the direction they wish to go //
+// Returns the winner function if the user chooses the right direction and the death2 function if wrong //
+// Used the textContent to change the text //
 const riddle3BridgeChoice = function() {
   textEl.textContent = "Very clever, you are. Now which path will you take? Choose the correct path and you may leave my realm.";
   btnContainer.style.display = "block";
@@ -193,6 +246,8 @@ const riddle3BridgeChoice = function() {
   });
 };
 
+// Create a function called death that ends the game if the user chooses a wrong answer //
+// Used the textContent to change the text //
 const death = function() {
   textEl.textContent = "The troll runs toward you, picks you up by the neck and tosses you off the rock. You have died.";
   btnContainer.style.display = "none";
@@ -202,6 +257,8 @@ const death = function() {
   textInput3.style.display = "none";
 };
 
+// Create a function called death2 that ends the game if the user chooses a wrong answer //
+// Used the textContent to change the text //
 const death2 = function() {
   textEl.textContent = "As you walk across the bridge it begins to disappear and you fall. You have died.";
   btnContainer.style.display = "none";
@@ -211,6 +268,8 @@ const death2 = function() {
   textInput3.style.display = "none";
 };
 
+// Create a function called winner that ends the game when the user chooses all the right answers //
+// Used the textContent to change the text //
 const winner = function() {
   textEl.textContent = "You walk over the bridge. Congratulations! You chose you correct bridge! You wake up in you bed and go back to your boring life. The end.";
   btnContainer.style.display = "none";
@@ -220,4 +279,5 @@ const winner = function() {
   textInput3.style.display = "none";
 };
 
+// Calls the start function to start the game //
 start();
